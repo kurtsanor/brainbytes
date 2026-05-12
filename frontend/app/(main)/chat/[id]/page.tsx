f86578b4445681a2fe1cd9099dfccc8982c218c4
Kurt Sanor <sanorkurtkelly@gmail.com>
2026-05-12 21:44:09 +0800

    redirect to new chat page if session id is invalid

--- frontend/app/(main)/chat/[id]/page.tsx
+++ frontend/app/(main)/chat/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getMessagesByChatId } from "@/lib/api/messages.server";
+import { redirect } from "next/navigation";
 import ChatContainer from "../chat-container";
 
 type Params = Promise<{ id: string }>;
@@ -6,9 +7,13 @@ type Params = Promise<{ id: string }>;
 const ChatPage = async ({ params }: { params: Params }) => {
   const { id } = await params;
 
-  const messages = await getMessagesByChatId(id);
+  try {
+    const messages = await getMessagesByChatId(id);
 
-  return <ChatContainer messages={messages} id={id} />;
+    return <ChatContainer messages={messages} id={id} />;
+  } catch (error) {
+    redirect("/chat");
+  }
 };
 
 export default ChatPage;
--- frontend/lib/api/messages.server.ts
+++ frontend/lib/api/messages.server.ts
@@ -3,12 +3,9 @@ import { apiServerFetch } from "./api-server";
 import { Chat } from "@/types/chat.types";
 
 export const getMessagesByChatId = async (chatId: string): Promise<Message[]> =>
-  apiServerFetch<{ messages: Message[] }>(`/api/chats/${chatId}/messages`)
-    .then((res) => res.messages)
-    .catch((error) => {
-      console.error("Error fetching messages:", error);
-      return [];
-    });
+  apiServerFetch<{ messages: Message[] }>(`/api/chats/${chatId}/messages`).then(
+    (res) => res.messages,
+  );
 
 export const getUserConversations = () =>
   apiServerFetch<{ data: Chat[] }>(`/api/chats/`, {
